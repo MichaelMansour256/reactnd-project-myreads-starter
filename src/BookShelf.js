d28fadec6f3f97bd34ca31e8a82bd086184e20b6
ghaly <ghalymansour9@gmail.com>
2021-11-23 06:16:58 +0200

    home and search functions done

---
+++ src/BookShelf.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Book from './Book.js';
+import PropTypes from 'prop-types';
+
+class BookShelf extends React.Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  }
+  render() {
+    const shelves = ["currentlyReading", "wantToRead", "read"]
+    const shelveNames = ["Currently Reading", "Want To Read", "Read"]
+    return (
+      <div>
+        {shelves.map((shelf, index) => {
+          return (
+            <div key={index} className="list-books-content">
+              <div>
+                <div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">{shelveNames[index]}</h2>
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                        {this.props.books
+                          .filter(book => book.shelf === shelf)
+                          .map(book => (
+                            <Book
+                              key={book.id}
+                              book={book}
+                            />
+                          ))
+                        }
+                      </ol>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        }
+        )}
+      </div>
+    )
+  }
+}
+
+export default BookShelf;
---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import BookShelf from './BookShelf';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+class Home extends React.Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  }
+
+  render() {
+    return (<div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <BookShelf books={this.props.books} />
+        </div>
+      </div>
+      <div className="open-search">
+        <Link to="/search"><button >Add a book</button></Link>
+      </div>
+    </div>);
+  }
+}
+
+export default Home;
